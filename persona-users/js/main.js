f89b7bff78a93348a862a034191dfdc19fc5ddf8
CrazyPaine <moarjocelyn@gmail.com>
2022-05-22 16:47:12 -0400

    Downloading
    Downloading files, hopefully it comes together.

---
+++ persona-users/js/main.js
@@ -0,0 +1,222 @@
+document.getElementById('joker').onclick = personaJoker
+document.getElementById('mona').onclick = personaMona
+document.getElementById('skull').onclick = personaSkull
+document.getElementById('panther').onclick = personaPanther
+document.getElementById('fox').onclick = personaFox
+document.getElementById('queen').onclick = personaQueen
+document.getElementById('oracle').onclick = personaOracle
+document.getElementById('noir').onclick = personaNoir
+document.getElementById('crow').onclick = personaCrow
+document.getElementById('violet').onclick = personaViolet
+document.getElementById('twins').onclick = personaTwins
+document.getElementById('secret').onclick = personaSecret
+document.getElementById('final').onclick = personaFinal
+
+function personaJoker() {
+    document.getElementById('mona').style.display = 'none';
+    document.getElementById('skull').style.display = 'none';
+    document.getElementById('panther').style.display = 'none';
+    document.getElementById('fox').style.display = 'none';
+    document.getElementById('queen').style.display = 'none';
+    document.getElementById('oracle').style.display = 'none';
+    document.getElementById('noir').style.display = 'none';
+    document.getElementById('crow').style.display = 'none';
+    document.getElementById('violet').style.display = 'none';
+    document.getElementById('twins').style.display = 'none';
+    document.getElementById('secret').style.display = 'none';
+    document.getElementById('final').style.display = 'none';
+   
+    alert ("You have selected Joker!")
+}
+
+function personaMona() {
+    document.getElementById('joker').style.display = 'none';
+    document.getElementById('skull').style.display = 'none';
+    document.getElementById('panther').style.display = 'none';
+    document.getElementById('fox').style.display = 'none';
+    document.getElementById('queen').style.display = 'none';
+    document.getElementById('oracle').style.display = 'none';
+    document.getElementById('noir').style.display = 'none';
+    document.getElementById('crow').style.display = 'none';
+    document.getElementById('violet').style.display = 'none';
+    document.getElementById('twins').style.display = 'none';
+    document.getElementById('secret').style.display = 'none';
+    document.getElementById('final').style.display = 'none';
+    alert ("You have selected Mona!")
+}
+
+function personaSkull() {
+    document.getElementById('joker').style.display = 'none';
+    document.getElementById('mona').style.display = 'none';
+    document.getElementById('panther').style.display = 'none';
+    document.getElementById('fox').style.display = 'none';
+    document.getElementById('queen').style.display = 'none';
+    document.getElementById('oracle').style.display = 'none';
+    document.getElementById('noir').style.display = 'none';
+    document.getElementById('crow').style.display = 'none';
+    document.getElementById('violet').style.display = 'none';
+    document.getElementById('twins').style.display = 'none';
+    document.getElementById('secret').style.display = 'none';
+    document.getElementById('final').style.display = 'none';
+    alert ("You have selected Skull!")
+}
+
+function personaPanther() {
+    document.getElementById('joker').style.display = 'none';
+    document.getElementById('skull').style.display = 'none';
+    document.getElementById('mona').style.display = 'none';
+    document.getElementById('fox').style.display = 'none';
+    document.getElementById('queen').style.display = 'none';
+    document.getElementById('oracle').style.display = 'none';
+    document.getElementById('noir').style.display = 'none';
+    document.getElementById('crow').style.display = 'none';
+    document.getElementById('violet').style.display = 'none';
+    document.getElementById('twins').style.display = 'none';
+    document.getElementById('secret').style.display = 'none';
+    document.getElementById('final').style.display = 'none';
+    alert ("You have selected Panther!")
+}
+function personaFox() {
+    document.getElementById('joker').style.display = 'none';
+    document.getElementById('skull').style.display = 'none';
+    document.getElementById('panther').style.display = 'none';
+    document.getElementById('mona').style.display = 'none';
+    document.getElementById('queen').style.display = 'none';
+    document.getElementById('oracle').style.display = 'none';
+    document.getElementById('noir').style.display = 'none';
+    document.getElementById('crow').style.display = 'none';
+    document.getElementById('violet').style.display = 'none';
+    document.getElementById('twins').style.display = 'none';
+    document.getElementById('secret').style.display = 'none';
+    document.getElementById('final').style.display = 'none';
+    alert ("You have selected Fox!")
+}
+
+function personaQueen() {
+    document.getElementById('joker').style.display = 'none';
+    document.getElementById('mona').style.display = 'none';
+    document.getElementById('skull').style.display = 'none';
+    document.getElementById('fox').style.display = 'none';
+    document.getElementById('panther').style.display = 'none';
+    document.getElementById('oracle').style.display = 'none';
+    document.getElementById('noir').style.display = 'none';
+    document.getElementById('crow').style.display = 'none';
+    document.getElementById('violet').style.display = 'none';
+    document.getElementById('twins').style.display = 'none';
+    document.getElementById('secret').style.display = 'none';
+    document.getElementById('final').style.display = 'none';
+    alert ("You have selected Queen!")
+}
+
+function personaOracle() {
+    document.getElementById('joker').style.display = 'none';
+    document.getElementById('mona').style.display = 'none';
+    document.getElementById('skull').style.display = 'none';
+    document.getElementById('panther').style.display = 'none';
+    document.getElementById('fox').style.display = 'none';
+    document.getElementById('queen').style.display = 'none';
+    document.getElementById('noir').style.display = 'none';
+    document.getElementById('crow').style.display = 'none';
+    document.getElementById('violet').style.display = 'none';
+    document.getElementById('twins').style.display = 'none';
+    document.getElementById('secret').style.display = 'none';
+    document.getElementById('final').style.display = 'none';
+    alert ("You have selected Oracle!")
+}
+
+function personaNoir() {
+    document.getElementById('joker').style.display = 'none';
+    document.getElementById('mona').style.display = 'none';
+    document.getElementById('skull').style.display = 'none';
+    document.getElementById('panther').style.display = 'none';
+    document.getElementById('fox').style.display = 'none';
+    document.getElementById('queen').style.display = 'none';
+    document.getElementById('oracle').style.display = 'none';
+    document.getElementById('crow').style.display = 'none';
+    document.getElementById('violet').style.display = 'none';
+    document.getElementById('twins').style.display = 'none';
+    document.getElementById('secret').style.display = 'none';
+    document.getElementById('final').style.display = 'none';
+    alert ("You have selected Noir!")
+}
+
+function personaCrow() {
+    document.getElementById('joker').style.display = 'none';
+    document.getElementById('mona').style.display = 'none';
+    document.getElementById('skull').style.display = 'none';
+    document.getElementById('panther').style.display = 'none';
+    document.getElementById('fox').style.display = 'none';
+    document.getElementById('queen').style.display = 'none';
+    document.getElementById('oracle').style.display = 'none';
+    document.getElementById('noir').style.display = 'none';
+    document.getElementById('violet').style.display = 'none';
+    document.getElementById('twins').style.display = 'none';
+    document.getElementById('secret').style.display = 'none';
+    document.getElementById('final').style.display = 'none';
+    alert ("You have selected Crow!")
+}
+
+function personaViolet() {
+    document.getElementById('joker').style.display = 'none';
+    document.getElementById('mona').style.display = 'none';
+    document.getElementById('skull').style.display = 'none';
+    document.getElementById('panther').style.display = 'none';
+    document.getElementById('fox').style.display = 'none';
+    document.getElementById('queen').style.display = 'none';
+    document.getElementById('oracle').style.display = 'none';
+    document.getElementById('noir').style.display = 'none';
+    document.getElementById('crow').style.display = 'none';
+    document.getElementById('twins').style.display = 'none';
+    document.getElementById('secret').style.display = 'none';
+    document.getElementById('final').style.display = 'none';
+    alert ("You have selected Violet!")
+}
+
+function personaTwins() {
+    document.getElementById('joker').style.display = 'none';
+    document.getElementById('mona').style.display = 'none';
+    document.getElementById('skull').style.display = 'none';
+    document.getElementById('panther').style.display = 'none';
+    document.getElementById('fox').style.display = 'none';
+    document.getElementById('queen').style.display = 'none';
+    document.getElementById('oracle').style.display = 'none';
+    document.getElementById('noir').style.display = 'none';
+    document.getElementById('crow').style.display = 'none';
+    document.getElementById('violet').style.display = 'none';
+    document.getElementById('secret').style.display = 'none';
+    document.getElementById('final').style.display = 'none';
+    alert ("You have selected Justine & Caroline!")
+}
+
+function personaSecret() {
+    document.getElementById('joker').style.display = 'none';
+    document.getElementById('mona').style.display = 'none';
+    document.getElementById('skull').style.display = 'none';
+    document.getElementById('panther').style.display = 'none';
+    document.getElementById('fox').style.display = 'none';
+    document.getElementById('queen').style.display = 'none';
+    document.getElementById('oracle').style.display = 'none';
+    document.getElementById('noir').style.display = 'none';
+    document.getElementById('crow').style.display = 'none';
+    document.getElementById('violet').style.display = 'none';
+    document.getElementById('twins').style.display = 'none';
+    document.getElementById('final').style.display = 'none';
+    alert ("You have selected Lavenza!")
+}
+
+function personaFinal() {
+    document.getElementById('joker').style.display = 'none';
+    document.getElementById('mona').style.display = 'none';
+    document.getElementById('skull').style.display = 'none';
+    document.getElementById('panther').style.display = 'none';
+    document.getElementById('fox').style.display = 'none';
+    document.getElementById('queen').style.display = 'none';
+    document.getElementById('oracle').style.display = 'none';
+    document.getElementById('noir').style.display = 'none';
+    document.getElementById('crow').style.display = 'none';
+    document.getElementById('violet').style.display = 'none';
+    document.getElementById('twins').style.display = 'none';
+    document.getElementById('secret').style.display = 'none';
+    alert ("You have selected Dr. Maruki!")
+}
+
